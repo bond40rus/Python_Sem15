c02e44d7efedcf4f150675efdfe623171b2cef87
bond40rus <116297875+bond40rus@users.noreply.github.com>
2023-12-19 21:38:14 +0300

    Add files via upload

---
+++ sem_15/student_log.py
@@ -0,0 +1,31 @@
+import argparse
+import logging
+from student_exeptions import StudentNameError, InvalidSubjectError, InvalidScoreError
+from student import Student
+
+logging.basicConfig(filename='studentlog', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process student information.')
+    parser.add_argument('name', help='Student name')
+    parser.add_argument('csv_filename', help='CSV filename')
+    args = parser.parse_args()
+
+    try:
+        student = Student(args.name, args.csv_filename)
+
+    except StudentNameError:
+        logging.error('Invalid student name format.')
+    except InvalidSubjectError as e:
+        logging.error(f'Invalid subject: {e.message}')
+    except InvalidScoreError as e:
+        logging.error(f'Invalid score: {e.message}')
+
+
+if __name__ == '__main__':
+    main()
+
+# для терминала
+# python student_log.py sergey subjects.csv - запуск из командной строки
+# Здесь student_log.py - имя файла со скриптом, sergey - имя студента, subjects.csv - имя CSV-файла с предметами."""
